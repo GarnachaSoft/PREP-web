c27ae0fccb2ad6ba494195a1ea4144e5cdf9d3c4
helmik <skator.kim@gmail.com>
2018-05-07 16:04:11 -0500

    Router redirect

---
+++ src/vuex/modules/session.js
@@ -0,0 +1,22 @@
+const state = {
+  user: {
+    email: '',
+    id: '',
+    token: ''
+  }
+}
+
+const getters = {
+  user: state => state.user
+}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
